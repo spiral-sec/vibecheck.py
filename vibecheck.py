a54f3ba84a58b811d1b45345a87f2c1130480656
bogdzn <wontsleep@protonmail.com>
2022-01-19 09:17:07 +0100

    (mod/display)support for colors to better differentiate categories

--- vibecheck.py
+++ vibecheck.py
@@ -4,6 +4,7 @@
 # If Python3 is not installed, you can use this script:
 # https://github.com/cervoise/linuxprivcheck/blob/master/linuxprivchecker3.py
 
+from colorama import Fore, Style
 import subprocess as s
 import sys
 
@@ -27,36 +28,36 @@ class Command:
     def run(self):
         self.output = self.run_command_wrapper()
         if self.output == '':
-            print(f' [-] Not output for {self.comment}')
+            print(f'{Fore.RED} [-] Not output for {self.comment}{Style.RESET_ALL}')
         else:
-            print(f' [+] {self.comment}:\n{self.output}')
+            print(f'{Fore.GREEN} [+] {self.comment}:\n{Style.RESET_ALL}{self.output}')
 
 
 #################################
 ######### ENUMERATION ###########
 #################################
 
-print("[*] System Info\n")
+print(f'{Fore.YELLOW}[*] System Info{Style.RESET_ALL}')
 Command("hostnamectl | grep 'Operating System | cut -d : -f 2'", "Operating System").run()
 Command("cat /proc/version", "Kernel version").run()
 Command("hostname", "Hostname").run()
 
-print("\n[*] Network Information\n")
+print(f'\n{Fore.YELLOW}[*] Network Info{Style.RESET_ALL}')
 Command("/sbin/ifconfig -a", "Network Interfaces", "ip address show").run()
 Command("route", "Route", "ip route").run()
 Command("netstat -antup | grep -v 'TIME_WAIT'",
         "Network status", "ss -lut | grep -v 'TIME_WAIT'").run()
 
-print("\n[*] Filesystem info\n")
+print(f'\n{Fore.YELLOW}[*] Filesystem Info{Style.RESET_ALL}')
 Command("mount", "mount output").run()
 Command("cat /etc/fstab 2>/dev/null", "fstab entries").run()
 
-print("\n[*] Cron jobs\n")
+print(f'\n{Fore.YELLOW}[*] Cron jobs{Style.RESET_ALL}')
 Command("ls -la /etc/cron* 2>/dev/null", "Scheduled cron jobs").run()
 Command("ls -laR /etc/cron 2>/dev/null | awk '$1 ~ /w.$/' 2>/dev/null",
         "Writable cron jobs").run()
 
-print("\n[*] Current user info\n")
+print(f'\n{Fore.YELLOW}[*] Current user info{Style.RESET_ALL}')
 Command("whoami", "Current user").run()
 Command("id", "Current user id").run()
 Command("cat /etc/passwd", "All users").run()
