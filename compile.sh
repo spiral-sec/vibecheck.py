58a000092f2054d81aa784147d9e03e1d4586455
winds0r <wontsleep@protonmail.com>
2022-05-26 20:04:04 +0200

    (mod/build)removing python version check

--- compile.sh
+++ compile.sh
@@ -4,33 +4,11 @@ set -euo pipefail
 
 PY_PATH=$(which python3 || which python)
 VENV_PATH="./venv"
-SUPPORTED_VERSION=3.9
 
 PIP_PACKAGES=("colorama==0.4.4" "staticx==0.13.6" "pyinstaller==5.1")
 SOURCES=("vibecheck.py")
 
-# Compares floating point numbers...i hate bash
-fcomp() {
-    awk -v n1="${1}" -v n2="${2}" 'BEGIN {if (n1+0<n2+0) exit 0; exit 1}'
-}
-
-check_python_version () {
-    [ -f "${PY_PATH}" ] || { echo "Could not find Python path" && exit 1; }
-    PYTHON_VERSION=$(${PY_PATH} --version | cut -d' ' -f2- - | cut -d'.' -f1-2 -)
-
-    echo "[+] Python installation: ${PY_PATH} [${PYTHON_VERSION}]"
-    if fcomp "${PYTHON_VERSION}" "${SUPPORTED_VERSION}"; then
-        echo "[!] Unsupported version."
-        echo "        Currently supported version: ${SUPPORTED_VERSION}"
-        echo "        Current Python version: ${PYTHON_VERSION}"
-        exit 1
-    fi
-}
-
-
 install_pip_packages () {
-    check_python_version
-
     ${PY_PATH} -m venv ${VENV_PATH}
     source ${VENV_PATH}/bin/activate
 
@@ -42,10 +20,7 @@ install_pip_packages () {
 
 
 cleanup () {
-    rm -vrf vibecheck.spec build vibecheck
-
-    mv dist/vibecheck .
-    rm -vrf dist
+    mv dist/vibecheck . && rm -vrf vibecheck.spec build dist
 }
 
 compile () {
