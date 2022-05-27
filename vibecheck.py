f4dbc765496ba083874465ada21b6f68a3169a78
winds0r <wontsleep@protonmail.com>
2022-05-27 10:20:16 +0200

    (fix/lint)escaped \ characters

--- vibecheck.py
+++ vibecheck.py
@@ -1,4 +1,4 @@
-#!/bin/python3.9
+#!/bin/python3.10
 
 # For simplicity reasons, this script will only run with Python3.
 # If Python3 is not installed, you can use this script:
@@ -20,15 +20,15 @@ class Command:
         self.alternative = alternative
         self.output = ''
 
-    def run_command_wrapper(self) -> str:
-        out, _ = s.Popen(
-                [self.command], stdout=s.PIPE, stderr=s.PIPE, shell=True
-                ).communicate()
-        if out == '' and self.alternative != '':
-            out, _ = s.Popen(
-                    [self.alternative], stdout=s.PIPE, stderr=s.PIPE, shell=True
-                    ).communicate()
+    def popen(self, command: str) -> str:
+        out, _ = s.Popen([command], stdout=s.PIPE, stderr=s.PIPE, shell=True).communicate()
         return out.decode('utf-8').strip()
+
+    def run_command_wrapper(self) -> str:
+        output = self.popen(self.command)
+        if output == '' and self.alternative != '':
+            output = self.popen(self.alternative)
+        return output
 
     def run(self):
         self.output = self.run_command_wrapper()
@@ -68,7 +68,7 @@ Command("doas -l", "Doas configuration").run()
 Command("id", "Current user id").run()
 Command("cat /etc/passwd", "All users").run()
 Command("grep -v -E '^#' /etc/passwd | awk -F: '$3 == 0{print $1}'", "Super users").run()
-Command("grep 'docker\|lxd' /etc/group", "Users in Docker group").run()
+Command("grep 'docker\\|lxd' /etc/group", "Users in Docker group").run()
 Command("env 2>/dev/null | grep -v 'LS_COLORS'", "Env values").run()
 Command("cat /etc/sudoers 2>dev/null | grep -v '#' 2>/dev/null", "sudoers file").run()
 Command("w 2>/dev/null", "user's activity").run()
@@ -79,15 +79,15 @@ Command("tmux ls 2>/dev/null", "Tmux active socket").run()
 
 print(f'\n{Color.YELLOW}[*] Programs information{Color.RESET_ALL}')
 Command("find / -perm -u=s -type f 2>/dev/null", "Check if there is programs with special perms").run()
-Command("find / \( -wholename '/home'homedir*' -prune \) \
-        -o \( -type d -perm -0002) -exec ls -ld '{}' ';' \
+Command("find / \\( -wholename '/home'homedir*' -prune \\) \
+        -o \\( -type d -perm -0002) -exec ls -ld '{}' ';' \
         2>/dev/null| grep root",
         "Writable directories for root group").run()
-Command("find / \( -wholename '/home/homedir/*' -prune -o \
-        -wholename '/proc/*' -prune \) -o \( -type f -perm -0002 \) \
+Command("find / \\( -wholename '/home/homedir/*' -prune -o \
+        -wholename '/proc/*' -prune \\) -o \\( -type f -perm -0002 \\) \
         -exec ls -l '{}' ';' 2>/dev/null",
         "Writable directories for users group").run()
-Command("find / \( -perm -2000 -o -perm -4000 \) -exec ls -ld {} \; 2>/dev/null",
+Command("find / \\( -perm -2000 -o -perm -4000 \\) -exec ls -ld {} \\; 2>/dev/null",
         "SUID/SGID files and directories").run()
 Command("ls -ahlR /root 2>/dev/null", "/root folder content").run()
 Command("getcap -r  / 2> /dev/null", "Checking capabilities").run()
